2fe4af361644079e090e5ed2f8d0d68481100532
Maxime Kestemont <maxime.kestemont@realimpactanalytics.com>
2016-04-18 23:21:14 +0200

    Tuto v0.5

---
+++ Assets/ZombieController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+
+	public float moveSpeed;
+
+	private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 currentPosition = transform.position;
+		
+		if( Input.GetButton("Fire1") ) {
+		  
+		  Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+		  
+		  moveDirection = moveToward - currentPosition;
+		  moveDirection.z = 0; 
+		  moveDirection.Normalize();
+		}
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+	}
+}
