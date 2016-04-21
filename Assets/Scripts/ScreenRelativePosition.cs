a322385518e5157f99ddc8560d51eab23c3d83ab
Maxime Kestemont <maxime.kestemont@realimpactanalytics.com>
2016-04-21 22:45:03 +0200

    Tuto v3.0

---
+++ Assets/Scripts/ScreenRelativePosition.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRelativePosition : MonoBehaviour {
+
+	public enum ScreenEdge {LEFT, RIGHT, TOP, BOTTOM};
+	public ScreenEdge screenEdge;
+	public float yOffset;
+	public float xOffset;
+
+	// Use this for initialization
+	void Start () {
+		// Only work with one camera
+		Vector3 newPosition = transform.position;
+		Camera camera = Camera.main;
+		 
+		// Assume de center of the screen is at (0, 0)
+		switch(screenEdge)
+		{
+		  case ScreenEdge.RIGHT:
+		  	// orthographicSize = half the view's height
+		    newPosition.x = camera.aspect * camera.orthographicSize + xOffset;
+		    newPosition.y = yOffset;
+		    break;
+		 
+		  case ScreenEdge.TOP:
+		    newPosition.y = camera.orthographicSize + yOffset;
+		    newPosition.x = xOffset;
+		    break;
+
+		  case ScreenEdge.LEFT:
+		    newPosition.x = -camera.aspect * camera.orthographicSize + xOffset;
+		    newPosition.y = yOffset;
+		    break;
+		    
+		  case ScreenEdge.BOTTOM:
+		    newPosition.y = -camera.orthographicSize + yOffset;
+		    newPosition.x = xOffset;
+		    break;
+		}
+		// 5
+		transform.position = newPosition;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -9,6 +9,10 @@ public class ZombieController : MonoBehaviour {
 
 	private Vector3 moveDirection;
 
+	[SerializeField]
+	private PolygonCollider2D[] colliders;
+	private int currentColliderIndex = 0;
+
 	// Use this for initialization
 	void Start () {
 		moveDirection = Vector3.right;
@@ -40,5 +44,52 @@ public class ZombieController : MonoBehaviour {
             Quaternion.Euler( 0, 0, targetAngle ), 
             turnSpeed * Time.deltaTime 
             );
+
+		EnforceBounds();
 	}
+
+
+	public void SetColliderForSprite( int spriteNum ) {
+		colliders[currentColliderIndex].enabled = false;
+	  	currentColliderIndex = spriteNum;
+	  	colliders[currentColliderIndex].enabled = true;
+	}
+
+
+	private void EnforceBounds() {
+	  	Vector3 newPosition = transform.position; 
+	  	Camera mainCamera = Camera.main;
+	  	Vector3 cameraPosition = mainCamera.transform.position;
+	 	
+	  	float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+	  	float xMax = cameraPosition.x + xDist;
+	  	float xMin = cameraPosition.x - xDist;
+	 	
+	  	// If the new position exceeds the horizontal bounds, inverse the direction
+	  	if ( newPosition.x < xMin || newPosition.x > xMax ) {
+	  	  	newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+	  	  	moveDirection.x = -moveDirection.x;
+	  	}
+
+		// Vertical bounds	  	
+	  	float yMax = mainCamera.orthographicSize;
+ 
+	 	if (newPosition.y < -yMax || newPosition.y > yMax) {
+  			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+  			moveDirection.y = -moveDirection.y;
+		}
+	 
+	  	// Update the position
+	  	transform.position = newPosition;
+	}
+
+
+
+	void OnTriggerEnter2D( Collider2D other ) {
+	  Debug.Log ("Hit " + other.gameObject);
+	}
+
+
+
+
 }
